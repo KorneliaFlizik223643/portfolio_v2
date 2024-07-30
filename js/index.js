9786416726e6693f545e6298984501ca695beb73
Kornelia Flizik <112756654+KorneliaFlizik223643@users.noreply.github.com>
2024-07-30 12:18:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-// JavaScript for Navbar
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav__link");
-
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
-});
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
-  });
-});
 
 // JavaScript for Scroll Animation
 const observer = new IntersectionObserver((entries) => {
